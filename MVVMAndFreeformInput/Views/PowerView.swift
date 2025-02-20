6f9d6e076dbf758e664dd58dd94812cf3089852d
Neil Shah <nshah@lcs.on.ca>
2025-02-20 09:48:54 -0500

    Organized the project so that there are groups for views, the model, and view models.

--- MVVMAndFreeformInput/Views/PowerView.swift
+++ MVVMAndFreeformInput/Views/PowerView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  PowerView.swift
 //  MVVMAndFreeformInput
 //
 //  Created by Neil Shah on 2025-02-20.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PowerView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    PowerView()
 }
